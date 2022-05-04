991631915a29f7c2b39370852d504b89d10637d9
ynsgndz <yns3gndz@hotmail.com.tr>
2022-05-04 14:14:47 +0300

    FRadioPlayer  kütüphanesini ekledim.   delegate olaylarının debuglarini ayarladım

--- yRadio/yRadio/ViewController.swift
+++ yRadio/yRadio/ViewController.swift
@@ -7,13 +7,107 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import FRadioPlayer
+
+
+class ViewController: UIViewController, FRadioPlayerDelegate {
+    func radioPlayer(_ player: FRadioPlayer, playerStateDidChange state: FRadioPlayerState) {
+        if state != nil{
+            print("Debug: -FRadioPlayerState \(state)")
+        }
+       
+    }
+    
+    func radioPlayer(_ player: FRadioPlayer, playbackStateDidChange state: FRadioPlaybackState) {
+        if state != nil{
+            print("Debug: -FRadioPlaybackState \(state)")
+        }
+    }
+    
+    func radioPlayer(_ player: FRadioPlayer, itemDidChange url: URL?){
+        if url != nil{print("Debug: -URL \(url!)")
+        }
+    }
+    
+    func radioPlayer(_ player: FRadioPlayer, metadataDidChange artistName: String?, trackName: String?){
+        if (artistName != nil) &&  trackName != nil{
+        print("Debug: -artistName \(artistName)")
+        print("Debug: -trackName \(trackName!)")
+        }
+    }
+    func radioPlayer(_ player: FRadioPlayer, metadataDidChange rawValue: String?){
+        if rawValue != nil{
+        print("Debug: -rawValue \(rawValue!)")
+        }
+    }
+    func radioPlayer(_ player: FRadioPlayer, artworkDidChange artworkURL: URL?){
+       
+        if artworkURL != nil{
+            print("Debug: -artworkURL \(artworkURL!)")
+        }
+    }
+    let player = FRadioPlayer.shared
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //MARK: - Radio
+        player.delegate = self
+        player.radioURL = URL(string: "https://listen.powerapp.com.tr/powerturk/mpeg/icecast.audio?/")
+
+       
+        
+        
+        
+        
         
     }
-
+    //MARK: - Tool Control
+    @IBAction func btnPlayPause(_ sender: Any) {
+        player.togglePlaying()
+    }
+    
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
